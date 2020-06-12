605a09f290ac536f2b35b3f777dccd2e946928cb
sean <yllekpnaes@gmail.com>
2020-06-12 16:48:02 -0400

    first deliverable

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Deliverable__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string VacationType;
+            int GroupSize;
+            string result;
+            Console.WriteLine("What kind of trip would you like to go on musical, tropical or adventurous? ");
+            string name = Console.ReadLine();
+
+            VacationType = "musical";
+            VacationType = "tropical";
+            VacationType = "adventurous";
+
+            Console.WriteLine("How many people are in your group for this trip?");
+            int classify;
+
+            (GroupSize<= 2);
+            
+                Console.WriteLine("helicopter");
+
+                int (3 =< 6);
+
+               Console.WriteLine("fly_first_class");
+
+            int (GroupSize >= 6) ;
+
+            Console.WriteLine("charter_a_flight");
+
+
+            Console.WriteLine("Where would you like to go?");
+            string name = Console.ReadLine();
+            string classify
+
+                string name = ("Musical" = "New Orleans")
+                string name = ("Tropical" = "Beach vacation in Mexico");
+            string name = ("Adventurous" = "Whitewater rafting the Grand Canyon");
+
+          
+            }
+    }
+}
